01b96b1b37fe982388f94ee4be5a2a09e3505465
Robert Röck <robert.roeck@robotron.de>
2020-11-16 13:20:55 +0100

    add css rules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HtmlComponent } from './components/codeConventions/html/html.component';
+import { HtmlComponent } from './views/codeConventions/html/html.component';
+import { CssComponent } from './views/codeConventions/css/css.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { JsComponent } from './views/codeConventions/js/js.component';
 
 const routes: Routes = [
   { path: 'code-convention/html', component: HtmlComponent },
+  { path: 'code-convention/css', component: CssComponent },
+  { path: 'code-convention/js', component: JsComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HtmlComponent
+    HtmlComponent,
+    CssComponent,
+    JsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/codeConventions/js/js.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-js',
+  templateUrl: './js.component.html',
+})
+export class JsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
