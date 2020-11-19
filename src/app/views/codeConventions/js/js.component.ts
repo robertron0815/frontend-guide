75f07ff9723a180204c018939febad76c0e93cda
Robert Röck <robert.roeck@robotron.de>
2020-11-19 11:36:41 +0100

    add js/ts guidelines

--- src/app/views/codeConventions/js/js.component.ts
+++ src/app/views/codeConventions/js/js.component.ts
@@ -6,9 +6,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JsComponent implements OnInit {
 
-  constructor() { }
+  greeting: string;
+
+  constructor() {
+
+    const values = ['VW', 'Audi', 'BMW', 'Mercedes Benz'];
+
+    const person = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 50,
+      eyeColor: 'blue'
+    };
+
+    console.log(values, person);
+
+    let x = 0;
+
+    for (let i = 0; i < 5; i++) {
+      x += i;
+    }
+
+    const time = 19;
+
+    if (time < 20) {
+      this.greeting = 'Good day';
+    } else {
+      this.greeting = 'Good evening';
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  /**
+   * what does the function do
+   * @param foo boolean
+   */
+  clickHandler = (foo: boolean): boolean => {
+    // do something
+    return foo;
   }
 
 }
